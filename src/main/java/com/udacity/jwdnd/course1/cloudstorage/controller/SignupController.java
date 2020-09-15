2fa6dfe91abd53e87ff1a57508f6dd70afae444e
Chao Lin <chao.lin0076@gmail.com>
2020-09-14 23:39:21 -0400

    add error page

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -40,6 +40,6 @@ public class SignupController {
         } else {
             model.addAttribute("signupError", signupError);
         }
-        return "signup";
+        return "login";
     }
 }
